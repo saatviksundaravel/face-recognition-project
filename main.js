e0bcb2ba51163c70aced8a7d5d2ab39dc216f974
saatviksundaravel <77335691+saatviksundaravel@users.noreply.github.com>
2021-07-09 11:14:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -23,5 +23,18 @@ function modelLoaded(){
 }
 
 function check(){
-    
+    img=document.getElementById('captured_image');
+    classifier.classify(img,gotresult);
+}
+
+function gotresult(error,results){
+    console.log("Image classified");
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML=results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(3);
+    }
 }
